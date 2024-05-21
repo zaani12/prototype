f3b323354fbc32f8835bb89e0c06bac2d1f3f2d7
Reda Grain <grain.reda.solicode@gmail.com>
2024-05-21 11:50:36 +0100

    u

--- app/app/Models/pkg_competences/CategorieTechnologie.php
+++ app/app/Models/pkg_competences/CategorieTechnologie.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\pkg_competences;
 
+use App\Models\Technologie;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
