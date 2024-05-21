a6bcf0434a93f864d53f45835e879f778b731ab0
imrane sarsri <imransarsi.ii@gmail.com>
2024-05-21 11:12:18 +0100

    save

--- app/app/Models/pkg_rh/Apprenant.php
+++ app/app/Models/pkg_rh/Apprenant.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\pkg_rh;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\pkg_notifications\Notification;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Apprenant extends Model
 {
     use HasFactory;
+
+    public function notification(){
+        return $this->hasMany(Notification::class,'apprenant_id');
+    }
 }
