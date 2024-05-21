6a9ab6a12f6b74290dee7c24ca48d849720865b8
husseinbouik <husseinbouik5@gmail.com>
2024-05-21 13:48:14 +0100

    Refactor namespace for Action model and add controller relationship

--- app/app/Models/pkg_autorisations/Action.php
+++ app/app/Models/pkg_autorisations/Action.php
@@ -4,6 +4,7 @@ namespace App\Models\pkg_autorisations;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\pkg_autorisations\Controller;
 
 class Action extends Model
 {
