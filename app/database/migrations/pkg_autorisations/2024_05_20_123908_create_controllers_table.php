f0b3268d1b8035bbdfddca26600c47993f610fbb
husseinbouik <husseinbouik5@gmail.com>
2024-05-21 13:15:52 +0100

    Refactor namespace for Autorisation models to pkg_autorisations

--- app/database/migrations/pkg_autorisations/2024_05_20_123908_create_controllers_table.php
+++ app/database/migrations/pkg_autorisations/2024_05_20_123908_create_controllers_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('controllers', function (Blueprint $table) {
             $table->id();
-            $table->string('nom');
             $table->timestamps();
         });
     }
