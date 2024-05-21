8f77c0f8841c2b9eb36a6af3aa6fe3cbc3dc7bd8
Reda Grain <grain.reda.solicode@gmail.com>
2024-05-21 23:53:33 +0200

    update

---
+++ app/database/migrations/pkg_projets/2024_05_21_213721_add_new_colomuns_to_tache_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('taches', function (Blueprint $table) {
+            $table->string('nom');
+            $table->text('description');
+            $table->string('priorité');
+            $table->Date('dateEchéance');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('taches', function (Blueprint $table) {
+            $table->dropColumn('nom');
+            $table->dropColumn('description');
+            $table->dropColumn('priorité');
+            $table->dropColumn('dateEchéance');
+        });
+    }
+};
