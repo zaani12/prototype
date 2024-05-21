7a54ad9341c80d6e31cffd2974f7e9e116498cdb
imrane sarsri <imransarsi.ii@gmail.com>
2024-05-21 12:57:23 +0100

    save

--- app/app/Models/pkg_notifications/Notification.php
+++ app/app/Models/pkg_notifications/Notification.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace App\Models\pkg_notifications;
+namespace App\Models;
 
-use App\Models\pkg_rh\Personne;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
--- app/app/Models/pkg_rh/Personne.php
+++ app/app/Models/pkg_rh/Personne.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace App\Models\pkg_rh;
+namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use App\Models\pkg_notifications\Notification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Personne extends Model
 {
