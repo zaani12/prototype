9856d56fb99777b579e39a4ce26f36bc5ff9c6eb
amine.lamchatab <alamchatab@gmail.com>
2024-05-21 13:05:03 +0100

    save

--- app/app/Models/pkg_autorisations/Controller.php
+++ app/app/Models/pkg_autorisations/Controller.php
@@ -2,10 +2,19 @@
 
 namespace App\Models\pkg_autorisations;
 
+use App\Models\Action;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Controller extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nom',
+    ];
+
+    public function actions(){
+        return $this->hasMany(Action::class,'controller_id');
+    }
 }
