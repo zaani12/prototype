3637aba7e64f7ea0a62ad59f786b47e9f91f8d28
husseinbouik <husseinbouik5@gmail.com>
2024-05-21 13:36:42 +0100

    Refactor namespace for Autorisation models to pkg_autorisations

--- app/app/Models/pkg_autorisations/Action.php
+++ app/app/Models/pkg_autorisations/Action.php
@@ -8,11 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Action extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'nom' ,
-        'controller_id',
-        'created_at',
-        'updated_at',
-        
-    ];
+
 }
