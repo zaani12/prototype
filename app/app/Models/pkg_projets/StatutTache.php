8ad20ce400f8c3fba6c77e28857154236f8fd61e
zaani12 <zaani.hamza.solicode@gmail.com>
2024-05-21 12:50:32 +0100

    Définir des modèles Laravel pour interagir avec la base de données

--- app/app/Models/pkg_projets/StatutTache.php
+++ app/app/Models/pkg_projets/StatutTache.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class StatutTache extends Model
 {
+   
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'nom',
+        'description',
+        'created_at',
+        'updated_at',
+    ];
+    public function taches()
+    {
+        return $this->hasMany(Tache::class);
+    }
 }
