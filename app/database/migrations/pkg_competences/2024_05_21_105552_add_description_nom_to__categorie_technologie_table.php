15c2e3d36d7f6f8bc513071ce7ce39c2f5d5f225
Reda Grain <grain.reda.solicode@gmail.com>
2024-05-21 11:58:49 +0100

    update

--- app/database/migrations/pkg_competences/2024_05_21_105552_add_description_nom_to__categorie_technologie_table.php
+++ app/database/migrations/pkg_competences/2024_05_21_105552_add_description_nom_to__categorie_technologie_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categorie_technologies', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('CategorieTechnologie', function (Blueprint $table) {
+            $table->string('nom');
+            $table->string('description');
         });
     }
 
@@ -22,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categorie_technologies');
+        Schema::table('CategorieTechnologie', function (Blueprint $table) {
+            $table->dropColumn('nom');
+            $table->dropColumn('description');
+        });
     }
 };
