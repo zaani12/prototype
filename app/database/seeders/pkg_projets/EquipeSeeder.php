a39d266a0f43993d00ac91a00eac9f0c3b31a31f
husseinbouik <husseinbouik5@gmail.com>
2024-05-21 10:51:04 +0100

    chore: Add seeders for notifications, posts, projets, and realisation_projet packages

---
+++ app/database/seeders/pkg_projets/EquipeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders\pkg_projets;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EquipeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
