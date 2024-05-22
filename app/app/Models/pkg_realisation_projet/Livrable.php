b23d2c83115e9cd166531e40442afda1c506c37f
safaa1faiz <Safaa1faiz@users.noreply.github.com>
2024-05-22 02:31:59 +0200

    save

--- app/app/Models/pkg_realisation_projet/Livrable.php
+++ app/app/Models/pkg_realisation_projet/Livrable.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\pkg_realisation_projet\NatureLivrable;
 
 class Livrable extends Model
 {
