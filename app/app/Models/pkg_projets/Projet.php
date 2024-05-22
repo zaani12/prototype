a93144dc5a8945c67b738cb3c6df8a6c6cafe3f6
zaani12 <zaani.hamza.solicode@gmail.com>
2024-05-22 09:13:51 +0100

    save

--- app/app/Models/pkg_projets/Projet.php
+++ app/app/Models/pkg_projets/Projet.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Projet extends Model
 {
     use HasFactory;
+    protected $fillable = ['nom', 'description', 'objectifs', 'date_debut', 'date_echeance'];
+    public function competences()
+    {
+        return $this->belongsToMany('App\Model\pkg_competences\Competence');
+    }
+
 }
