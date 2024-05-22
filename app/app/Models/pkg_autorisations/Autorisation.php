52ddaf02f7d94e0284864549493ea57f71606ee7
husseinbouik <husseinbouik5@gmail.com>
2024-05-22 15:03:36 +0100

    Refactor Autorisation model to include fillable fields

--- app/app/Models/pkg_autorisations/Autorisation.php
+++ app/app/Models/pkg_autorisations/Autorisation.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Autorisation extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'action_id',
+        'role_id',
+        'created_at',
+        'updated_at',
+    ]; 
 }
