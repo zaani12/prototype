ae787a28fae1f54c5defee6fc1a6c8cbce88778d
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-05-23 17:00:23 +0100

    update menu

--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -10,3 +10,7 @@
 @include('layouts.GestionProjets.GestionProjetsMenu')
 @include('layouts.pkg_rh.pkg_rhMenu')
 @include('layouts.pkg_realisation_projet.GestionLivrable')
+
+
+@include('layouts.pkg-autorisations.GestionAutorisation')
+@include('layouts.pkg-autorisations.GestionProjets')
