eacb9f649312a8654a06344ac1bdb1d306e24b7f
= <=>
2024-05-23 11:44:35 +0100

    save

--- app/database/seeders/pkg_rh/PersonneSeeder.php
+++ app/database/seeders/pkg_rh/PersonneSeeder.php
@@ -18,23 +18,23 @@ class PersonneSeeder extends Seeder
     public function run(): void
     {
 
-        Schema::disableForeignKeyConstraints();
-        Personne::truncate();
-        Schema::enableForeignKeyConstraints();
+        // Schema::disableForeignKeyConstraints();
+        // Personne::truncate();
+        // Schema::enableForeignKeyConstraints();
 
-        $csvFile = fopen(base_path("database/data/pkg_rh/Personnes.csv"), "r");
-        $firstline = true;
-        while (($data = fgetcsv($csvFile)) !== FALSE) {
-            if (!$firstline) {
-                Personne::create([
-                    "nom"=>$data['0'],
-                    "prenom"=>$data['1'],
-                    "type"=>$data["2"],
-                    "groupe_id"=>(int) $data["5"]
+        // $csvFile = fopen(base_path("database/data/pkg_rh/Personnes.csv"), "r");
+        // $firstline = true;
+        // while (($data = fgetcsv($csvFile)) !== FALSE) {
+        //     if (!$firstline) {
+        //         Personne::create([
+        //             "nom"=>$data['0'],
+        //             "prenom"=>$data['1'],
+        //             "type"=>$data["2"],
+        //             "groupe_id"=>(int) $data["5"]
                   
-            $firstline = false;
-        }
+        //     $firstline = false;
+        // }
 
-        fclose($csvFile);
+        // fclose($csvFile);
     }
 }
