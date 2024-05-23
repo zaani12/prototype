d6aeb69e9a8127bce2d23de8ba40f9162e1bbea5
Amina Assaid <aminaassaid123@gmail.com>
2024-05-23 01:04:44 +0100

    fix seeders

--- app/database/seeders/pkg_realisation_projet/MessageSeeder.php
+++ app/database/seeders/pkg_realisation_projet/MessageSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders\pkg_realisation_projet;
 
-use App\Models\pkg_projets\Projet;
-use App\Models\pkg_projets\Tache;
 use App\Models\pkg_realisation_projet\Message;
 use Illuminate\Database\Seeder;
 
@@ -11,36 +9,32 @@ class MessageSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
         $csvFile = fopen(base_path('database/data/pkg_realisation_projet/messages.csv'), 'r');
         if ($csvFile === false) {
             throw new \Exception('Could not open the CSV file.');
         }
 
-        // $csvFile = fopen(base_path('database/data/pkg_realisation_projet/messages.csv'), 'r');
-        // if ($csvFile === false) {
-        //     throw new \Exception('Could not open the CSV file.');
-        // }
-
-
-        // $firstline = true;
-        // while (($data = fgetcsv($csvFile, 1000, ",")) !== FALSE) {
-        //     if (!$firstline) {
-
-        //         Message::create([
-        //             'titre' => $data[0],
-        //             'description' => $data[1],
-        //             'projet_id' => $data[2],
-        //             'tach_id' => $data[3],
-        //             'isLue' => filter_var($data[4], FILTER_VALIDATE_BOOLEAN),
-        //         ]);
-        //     }
-        //     $firstline = false;
-        // }
-
-
-        // fclose($csvFile);
+        try {
+            $firstline = true;
+            while (($data = fgetcsv($csvFile, 1000, ",")) !== false) {
+                if (!$firstline) {
+                    Message::create([
+                        'titre' => $data[0],
+                        'description' => $data[1],
+                        'projet_id' => $data[2],
+                        'tach_id' => $data[3],
+                        'isLue' => filter_var($data[4], FILTER_VALIDATE_BOOLEAN),
+                    ]);
+                }
+                $firstline = false;
+            }
+        } finally {
+            fclose($csvFile);
+        }
     }
 }
