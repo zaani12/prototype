3b29739e68f61e57ddfca99a1b4c673384712cbc
Amina Assaid <aminaassaid123@gmail.com>
2024-05-23 11:24:38 +0100

    save

--- app/app/Http/Controllers/pkg_competences/CompetenceController.php
+++ app/app/Http/Controllers/pkg_competences/CompetenceController.php
@@ -17,6 +17,7 @@ use Maatwebsite\Excel\Facades\Excel;
 class CompetenceController extends AppBaseController
 {
     protected $competenceRepository;
+
     public function __construct(CompetenceRepository $competenceRepository)
     {
         $this->competenceRepository = $competenceRepository;
@@ -24,7 +25,6 @@ class CompetenceController extends AppBaseController
 
     public function index(Request $request)
     {
-
         if ($request->ajax()) {
             $searchValue = $request->get('searchValue');
             if ($searchValue !== '') {
@@ -37,22 +37,18 @@ class CompetenceController extends AppBaseController
         return view('pkg_competences.competence.index', compact('competenceData'));
     }
 
-
     public function create()
     {
         $dataToEdit = null;
         return view('pkg_competences.competence.create', compact('dataToEdit'));
     }
 
-
     public function store(CompetenceRequest $request)
     {
-
         try {
             $validatedData = $request->validated();
             $this->competenceRepository->create($validatedData);
-            return redirect()->route('competences.index')->with('success',__('pkg_competences/competence.singular').' '.__('app.addSucées'));
-
+            return redirect()->route('competences.index')->with('success', __('pkg_competences/competence.singular') . ' ' . __('app.addSuccess'));
         } catch (CompetenceAlreadyExistException $e) {
             return back()->withInput()->withErrors(['competence_exists' => __('pkg_competences/competence/message.createProjectException')]);
         } catch (\Exception $e) {
@@ -60,46 +56,39 @@ class CompetenceController extends AppBaseController
         }
     }
 
-
     public function show(string $id)
     {
         $fetchedData = $this->competenceRepository->find($id);
-        return view('pkg_competences.projet.show', compact('fetchedData'));
+        return view('pkg_competences.competence.show', compact('fetchedData'));
     }
-
 
     public function edit(string $id)
     {
-        $dataToEdit = $this->projectRepository->find($id);
-        $dataToEdit->date_debut = Carbon::parse($dataToEdit->date_debut)->format('Y-m-d');
-        $dataToEdit->date_de_fin = Carbon::parse($dataToEdit->date_de_fin)->format('Y-m-d');
+        $dataToEdit = $this->competenceRepository->find($id);
+        $dataToEdit->nom = Carbon::parse($dataToEdit->nom);
+        $dataToEdit->description = Carbon::parse($dataToEdit->description);
 
-        return view('GestionProjets.projet.edit', compact('dataToEdit'));
+        return view('pkg_competences.competence.edit', compact('dataToEdit'));
     }
 
-
-    public function update(projetRequest $request, string $id)
+    public function update(CompetenceRequest $request, string $id)
     {
         $validatedData = $request->validated();
-        $this->projectRepository->update($id, $validatedData);
-        return redirect()->route('projets.index', $id)->with('success',__('GestionProjets/projet.singular').' '.__('app.updateSucées'));
+        $this->competenceRepository->update($id, $validatedData);
+        return redirect()->route('competences.index')->with('success', __('pkg_competences/competence.singular') . ' ' . __('app.updateSuccess'));
     }
-
 
     public function destroy(string $id)
     {
-        $this->projectRepository->destroy($id);
-        return redirect()->route('projets.index')->with('success', 'Le projet a été supprimer avec succés.');
+        $this->competenceRepository->destroy($id);
+        return redirect()->route('competences.index')->with('success', 'Le projet a été supprimé avec succès.');
     }
-
 
     public function export()
     {
-        $projects = projet::all();
-
-        return Excel::download(new ProjetExport($projects), 'projet_export.xlsx');
+        $competences = Competence::all();
+        return Excel::download(new CompetenceExport($competences), 'competence_export.xlsx');
     }
-
 
     public function import(Request $request)
     {
@@ -108,10 +97,10 @@ class CompetenceController extends AppBaseController
         ]);
 
         try {
-            Excel::import(new ProjetImport, $request->file('file'));
+            Excel::import(new CompetenceImport, $request->file('file'));
         } catch (\InvalidArgumentException $e) {
-            return redirect()->route('projets.index')->withError('Le symbole de séparation est introuvable. Pas assez de données disponibles pour satisfaire au format.');
+            return redirect()->route('competences.index')->withError('Le symbole de séparation est introuvable. Pas assez de données disponibles pour satisfaire au format.');
         }
-        return redirect()->route('projets.index')->with('success',__('GestionProjets/projet.singular').' '.__('app.addSucées'));
+        return redirect()->route('competences.index')->with('success', __('pkg_competences/competence.singular') . ' ' . __('app.addSuccess'));
     }
 }
