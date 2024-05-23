7220743dd4c602678f05a4ec77764e8b88418d7f
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2024-05-23 12:13:35 +0100

    save work

---
+++ app/app/Repositories/pkg_rh/ApprenantRepositorie.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Repositories\pkg_rh;
+
+use App\Repositories\BaseRepository;
+use App\Models\pkg_rh\Apprenant;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class ApprenantRepositorie extends BaseRepository
+{
+    protected $type;
+
+    /**
+     * Les champs de recherche disponibles pour les projets.
+     *
+     * @var array
+     */
+    protected $fieldsSearchable = [
+        'name'
+    ];
+
+    /**
+     * Renvoie les champs de recherche disponibles.
+     *
+     * @return array
+     */
+    public function getFieldsSearchable(): array
+    {
+        return $this->fieldsSearchable;
+    }
+
+    /**
+     * Constructeur de la classe ProjetRepository.
+     */
+    public function __construct($type = null)
+    {
+        $this->type = $type;
+        parent::__construct(new Apprenant());
+    }
+
+    public function paginate($search = [], $perPage = 0, array $columns = ['*']): LengthAwarePaginator
+    {
+        if ($this->type !== null) {
+            return $this->model->where('type', $this->type)->paginate($perPage, $columns);
+        } else {
+            return $this->model->paginate($perPage, $columns);
+        }
+    }
+
+    /**
+     * Recherche apprenants correspondants aux critères spécifiés.
+     *
+     * @param mixed $searchableData Données de recherche.
+     * @param int $perPage Nombre d'éléments par page.
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function searchData($searchableData, $perPage = 4)
+    {
+        return $this->model->where('type', 'Apprenant')->where(function($query) use ($searchableData) {
+            $query->where('nom', 'like', '%' . $searchableData . '%')
+                  ->orWhere('prenom', 'like', '%' . $searchableData . '%');
+        })->paginate($perPage);
+    }
+}
