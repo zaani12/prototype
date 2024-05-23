cc0cdade222981ed316ee3690203689c6da723eb
imrane sarsri <imransarsi.ii@gmail.com>
2024-05-23 09:32:43 +0100

    fix error seeder pkg_rh and pkg_notification

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 
 use Database\Seeders\{
     UserSeeder ,
-    CompetencesSeeder, 
+    CompetencesSeeder,
     NotificationsSeeder,
     AutorisationsSeeder,
     RHSeeder ,
@@ -20,11 +20,11 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->call(RHSeeder::class);
         $this->call(AutorisationsSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(CompetencesSeeder::class);
         $this->call(NotificationsSeeder::class);
-        $this->call(RHSeeder::class);
         $this->call(ProjetsSeeder::class);
         $this->call(RealisationProjetSeeder::class);
         $this->call(PostsSeeder::class);
---
+++ app/database/seeders/pkg_rh/PersonneSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders\pkg_rh;
+
+use App\Models\pkg_rh\Personne;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class PersonneSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+
+
+        Schema::disableForeignKeyConstraints();
+        Personne::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $csvFile = fopen(base_path("database/data/pkg_rh/Personnes.csv"), "r");
+        $firstline = true;
+
+        while (($data = fgetcsv($csvFile)) !== FALSE) {
+            if (!$firstline) {
+                Personne::create([
+                    "nom" => $data['0'],
+                    "prenom" => $data['1'],
+                    "type" => $data['2']
+                ]);
+            }
+            $firstline = false;
+        }
+
+        fclose($csvFile);
+    }
+}
