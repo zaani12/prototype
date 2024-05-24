ad0770f0343d0ea98deb29f877bd0874f3946f8e
Amina Assaid <aminaassaid123@gmail.com>
2024-05-24 16:26:34 +0100

    save

--- app/resources/views/layouts/pkg_competences/CompetencesMenu.blade.php
+++ app/resources/views/layouts/pkg_competences/CompetencesMenu.blade.php
@@ -8,31 +8,10 @@
     </a>
     <ul class="nav nav-treeview">
         <li class="nav-item ">
-            <a href="{{ route('technologie.index') }}"
-                class="nav-link nav-link {{ Request::is('technologie*') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
-                <p>Technologies</p>
-            </a>
-        </li>
-        <li class="nav-item ">
-            <a href="{{ route('niveauxCompetences.index') }}"
-                class="nav-link nav-link {{ Request::is('niveauxCompetences*') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
-                <p>Niveaux competences</p>
-            </a>
-        </li>
-        <li class="nav-item ">
             <a href="{{ route('competence.index') }}"
                 class="nav-link nav-link {{ Request::is('competence*') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-table"></i>
                 <p>Gestion Competences</p>
-            </a>
-        </li>
-        <li class="nav-item ">
-            <a href="{{ route('CategorieTechnologie.index') }}"
-                class="nav-link nav-link {{ Request::is('CategorieTechnologie*') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
-                <p>Categorie technologie</p>
             </a>
         </li>
     </ul>
