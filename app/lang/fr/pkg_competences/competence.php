de8732112ad92c5aac1be56aa57cf30bcb81f3eb
Amina Assaid <aminaassaid123@gmail.com>
2024-05-24 05:21:27 +0100

    Save

--- app/lang/fr/pkg_competences/competence.php
+++ app/lang/fr/pkg_competences/competence.php
@@ -3,10 +3,6 @@
 return [
     'singular' => 'Competence',
     'plural' => 'Competences',
-    'name' => 'Nom',
-    'prenom' => 'Prenom',
-    'action'=> 'Action',
-    'type' => 'Type',
-    
+
 ];
 
