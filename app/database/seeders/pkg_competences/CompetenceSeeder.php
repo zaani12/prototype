899478b57e58e182be60df04bb0cde0fcdd08807
Amina Assaid <aminaassaid123@gmail.com>
2024-05-24 14:27:00 +0100

    save

--- app/database/seeders/pkg_competences/CompetenceSeeder.php
+++ app/database/seeders/pkg_competences/CompetenceSeeder.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace Database\Seeders\pkg_competences;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use App\Models\User;
 use App\Models\pkg_competences\Competence;
 use App\Models\pkg_competences\NiveauCompetence;
 use Illuminate\Support\Facades\Schema;
@@ -41,3 +43,38 @@ class CompetenceSeeder extends Seeder
         fclose($csvFile);
     }
 }
+
+        // ==========================================================
+        // =========== Add Seeder Permission Assign Role ============
+        // ==========================================================
+        $FormateurRole = User::FORMATEUR;
+        $Role = Role::where('name', $FormateurRole)->first();
+
+        Schema::disableForeignKeyConstraints();
+        Permission::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $csvFile = fopen(base_path("database/data/pkg_competences/CompetencePermission.csv"), "r");
+        $firstline = true;
+        while (($data = fgetcsv($csvFile)) !== FALSE) {
+            if (!$firstline) {
+                Permission::create([
+                    "name" => $data['0'],
+                    "guard_name" => $data['1'],
+                ]);
+
+                if ($Role) {
+                    // If the role exists, update its permissions
+                    $Role->givePermissionTo($data['0']);
+                } else {
+                    // If the role doesn't exist, create it and give permissions
+                    $Role = Role::create([
+                        'name' => $FormateurRole,
+                        'guard_name' => 'web',
+                    ]);
+                    $Role->givePermissionTo($data['0']);
+                }
+            }
+            $firstline = false;
+        }
+        fclose($csvFile);
