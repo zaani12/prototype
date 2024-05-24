9b175e9549cb23c774b996355dde0209df0dfb6c
jalil betroji <jalil.betroji@gmail.com>
2024-05-24 15:12:12 +0100

    menu updated

--- app/resources/views/layouts/pkg_competences/CategorieTechnologieMenu.blade.php
+++
@@ -1,9 +0,0 @@
-<ul class="nav nav-treeview">
-    <li class="nav-item ">
-        <a href="{{ route('technologie.index') }}"
-            class="nav-link nav-link {{ Request::is('technologie*') ? 'active' : '' }}">
-            <i class="nav-icon fas fa-table"></i>
-            <p>Technologies</p>
-        </a>
-    </li>
-</ul>
