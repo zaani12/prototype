643a0886ee3e7368e73bc59b49de3bf73055af9c
imrane sarsri <imransarsi.ii@gmail.com>
2024-05-24 16:01:34 +0100

    save

--- app/resources/views/layouts/pkg_competences/CompetencesMenu.blade.php
+++ app/resources/views/layouts/pkg_competences/CompetencesMenu.blade.php
@@ -8,7 +8,7 @@
     </a>
     <ul class="nav nav-treeview">
         <li class="nav-item ">
-            <a href="{{ route('technologie.index') }}"
+            <a href="{{ route('technologies.index') }}"
                 class="nav-link nav-link {{ Request::is('technologie*') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-table"></i>
                 <p>Technologies</p>
