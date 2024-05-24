89a614349c8071799530da5830a6b24cd0b72d27
imrane sarsri <imransarsi.ii@gmail.com>
2024-05-24 15:13:05 +0100

    save

--- app/resources/views/layouts/pkg_rh/pkg_rhMenu.blade.php
+++ app/resources/views/layouts/pkg_rh/pkg_rhMenu.blade.php
@@ -1,6 +1,5 @@
-
 <li class="nav-item has-treeview">
-    <a href="#" class="nav-link nav-link {{ Request::is('projets*') ? 'active' : '' }}">
+    <a href="#" class="nav-link nav-link {{ Request::is('rh*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-table"></i>
         <p>
             Gestion de RH
@@ -9,13 +8,15 @@
     </a>
     <ul class="nav nav-treeview">
         <li class="nav-item ">
-            <a href="{{ route('apprenant.index') }}" class="nav-link nav-link {{ Request::is('apprenant*') ? 'active' : '' }}">
+            <a href="{{ route('apprenant.index') }}"
+                class="nav-link nav-link {{ Request::is('apprenant*') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-table"></i>
                 <p>Apprenant</p>
             </a>
         </li>
         <li class="nav-item ">
-            <a href="{{ route('formateur.index') }}" class="nav-link nav-link {{ Request::is('formateur*') ? 'active' : '' }}">
+            <a href="{{ route('formateur.index') }}"
+                class="nav-link nav-link {{ Request::is('formateur*') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-table"></i>
                 <p>Formateur</p>
             </a>
@@ -23,4 +24,3 @@
     </ul>
 
 </li>
-
