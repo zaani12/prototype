dbd4d29e070a4c65ef2abd50aadf8cd21cd6a5b0
Amina Assaid <aminaassaid123@gmail.com>
2024-05-24 05:17:25 +0100

    gestion competences

--- app/lang/fr/pkg_competences/competence.php
+++ app/lang/fr/pkg_competences/competence.php
@@ -3,4 +3,10 @@
 return [
     'singular' => 'Competence',
     'plural' => 'Competences',
+    'name' => 'Nom',
+    'prenom' => 'Prenom',
+    'action'=> 'Action',
+    'type' => 'Type',
+    
 ];
+
--- app/routes/pkg_competences/CompetenceRoute.php
+++ app/routes/pkg_competences/CompetenceRoute.php
@@ -6,9 +6,16 @@ use App\Models\pkg_competences\Competence;
 
 // routes for competence management
 Route::middleware('auth')->group(function () {
-    Route::prefix('/')->group(function () {
-        Route::resource('competences', CompetenceController::class);
-        Route::get('competences/export', [CompetenceController::class, 'export'])->name('competences.export');
-        Route::post('competences/import', [CompetenceController::class, 'import'])->name('competences.import');
+    $namespace = 'App\Http\Controllers\pkg_competences';
+    Route::namespace($namespace)->group(function () {
+        Route::prefix('Competence')->group(function () {
+            Route::get('/', [CompetenceController::class, 'index'])->name('competence.index');
+            Route::get('/form-ajouter', [CompetenceController::class, 'create'])->name('competence.create');
+            Route::post('/ajouter', [CompetenceController::class, 'store'])->name('competence.store');
+            Route::get('/{id}', [CompetenceController::class, 'show'])->name('competence.show');
+            Route::get('/{id}/edit', [CompetenceController::class, 'edit'])->name('competence.edit');
+            Route::post('/{id}/update', [CompetenceController::class, 'update'])->name('competence.update');
+            Route::delete('/{id}/delete', [CompetenceController::class, 'destroy'])->name('competence.delete');
+        });
     });
 });
