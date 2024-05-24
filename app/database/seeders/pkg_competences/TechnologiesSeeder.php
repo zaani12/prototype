1a0f8b69c8a2d7d883687c251fbff3fbdba314dc
imrane sarsri <imransarsi.ii@gmail.com>
2024-05-24 10:10:10 +0100

    add roles

--- app/database/seeders/pkg_competences/TechnologiesSeeder.php
+++ app/database/seeders/pkg_competences/TechnologiesSeeder.php
@@ -2,13 +2,15 @@
 
 namespace Database\Seeders\pkg_competences;
 
-use App\Models\pkg_competences\Technologie;
 use Carbon\Carbon;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Schema;
-use App\Models\pkg_notifications\Notification;
 use Spatie\Permission\Models\Permission;
+use App\Models\pkg_competences\Technologie;
+use App\Models\pkg_notifications\Notification;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Role;
 
 class TechnologiesSeeder extends Seeder
 {
@@ -18,13 +20,15 @@ class TechnologiesSeeder extends Seeder
     public function run(): void
     {
 
+        // ==========================================================
+        // ================= Add Seeder Technologies ================
+        // ==========================================================
         Schema::disableForeignKeyConstraints();
         Technologie::truncate();
         Schema::enableForeignKeyConstraints();
 
         $csvFile = fopen(base_path("database/data/pkg_competences/technologies/Technologies.csv"), "r");
         $firstline = true;
-        $i = 0;
         while (($data = fgetcsv($csvFile)) !== FALSE) {
             if (!$firstline) {
                 Technologie::create([
@@ -39,19 +43,36 @@ class TechnologiesSeeder extends Seeder
         fclose($csvFile);
 
 
+        // ==========================================================
+        // =========== Add Seeder Permission Assign Role ============
+        // ==========================================================
+        $FormateurRole = User::FORMATEUR;
+        $Role = Role::where('name', $FormateurRole)->first();
+
         Schema::disableForeignKeyConstraints();
         Permission::truncate();
         Schema::enableForeignKeyConstraints();
 
         $csvFile = fopen(base_path("database/data/pkg_competences/technologies/TechnologiesPermissions.csv"), "r");
         $firstline = true;
-        $i = 0;
         while (($data = fgetcsv($csvFile)) !== FALSE) {
             if (!$firstline) {
                 Permission::create([
                     "name" => $data['0'],
                     "guard_name" => $data['1'],
                 ]);
+
+                if ($Role) {
+                    // If the role exists, update its permissions
+                    $Role->givePermissionTo($data['0']);
+                } else {
+                    // If the role doesn't exist, create it and give permissions
+                    $Role = Role::create([
+                        'name' => $FormateurRole,
+                        'guard_name' => 'web',
+                    ]);
+                    $Role->givePermissionTo($data['0']);
+                }
             }
             $firstline = false;
         }
