2b05dd555c256e73a87cd13ce70729617f481cbc
imrane sarsri <imransarsi.ii@gmail.com>
2024-05-24 15:42:11 +0100

    save

--- app/routes/pkg_competences/TechnologieRoute.php
+++ app/routes/pkg_competences/TechnologieRoute.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Pkg_competences\TechnologieController;
 
 // routes for project management
 Route::middleware('auth')->group(function () {
-    Route::prefix('/technologie')->group(function () {
+    Route::prefix('/competences')->group(function () {
         Route::get('technologies/export', [TechnologieController::class, 'export'])->name('technologies.export');
         Route::post('technologies/import', [TechnologieController::class, 'import'])->name('technologies.import');
         Route::resource('technologies', TechnologieController::class);
