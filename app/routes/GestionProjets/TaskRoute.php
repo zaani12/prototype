cf8213012fc94179454a932875166da6df7da750
Reda Grain <grain.reda.solicode@gmail.com>
2024-05-24 12:37:15 +0100

    u

--- app/routes/GestionProjets/TaskRoute.php
+++ app/routes/GestionProjets/TaskRoute.php
@@ -1,13 +1,13 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\GestionTasks\TaskController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GestionTasks\TaskController;
 
-// // routes for tasks management
-// Route::middleware('auth')->group(function () {
-//     Route::prefix('/tasks')->group(function () {
-//         Route::resource('tasks', TaskController::class);
-//         Route::get('tasks/export', [TaskController::class, 'export'])->name('tasks.export');
-//         Route::post('tasks/import', [TaskController::class, 'import'])->name('tasks.import');
-//     });
-// });
+// routes for tasks management
+Route::middleware('auth')->group(function () {
+    Route::prefix('/tasks')->group(function () {
+        Route::resource('tasks', TaskController::class);
+        Route::get('tasks/export', [TaskController::class, 'export'])->name('tasks.export');
+        Route::post('tasks/import', [TaskController::class, 'import'])->name('tasks.import');
+    });
+});
