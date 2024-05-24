78ddafc471b097b61275991c82b6a821b470b065
Reda Grain <grain.reda.solicode@gmail.com>
2024-05-24 15:44:42 +0100

    update

--- app/routes/pkg_competences/CategorieTechnologieRoute.php
+++ app/routes/pkg_competences/CategorieTechnologieRoute.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\pkg_competences\CategorieTechnologieController;
 
 Route::middleware('auth')->group(function () {
-    Route::put('CategorieTechnologie/{CategorieTechnologie}/update', [CategorieTechnologieController::class, 'update'])->name('CategorieTechnologie.update');
-    Route::get('CategorieTechnologie/export', [CategorieTechnologieController::class, 'export'])->name('CategorieTechnologie.export');
-    Route::post('CategorieTechnologie/import', [CategorieTechnologieController::class, 'import'])->name('CategorieTechnologie.import');
-    Route::resource('CategorieTechnologie', CategorieTechnologieController::class);
+    Route::prefix('/competences')->group(function () {
+        Route::put('/CategorieTechnologie/{CategorieTechnologie}/update', [CategorieTechnologieController::class, 'update'])->name('CategorieTechnologie.update');
+        Route::get('/CategorieTechnologie/export', [CategorieTechnologieController::class, 'export'])->name('CategorieTechnologie.export');
+        Route::post('/CategorieTechnologie/import', [CategorieTechnologieController::class, 'import'])->name('CategorieTechnologie.import');
+        Route::resource('/CategorieTechnologie', CategorieTechnologieController::class);
+    });
 });
