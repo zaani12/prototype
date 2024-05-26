d3c7d7599c4bfbc1263da9352c96820ab79f5ce7
ADNANLH <adnanharrak5@gmail.com>
2024-05-26 01:07:10 +0100

    uo

--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -11,7 +11,3 @@
 @include('layouts.pkg_competences.CompetencesMenu')
 @include('layouts.pkg_rh.pkg_rhMenu')
 @include('layouts.pkg_realisation_projet.GestionLivrable')
-
-
-@include('layouts.pkg-autorisations.GestionAutorisation')
-@include('layouts.pkg-autorisations.GestionProjets')
